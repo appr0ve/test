1f4a69aea1e0adea8382a2e8d9f3ebe341bc916b
appr0ve <185701845+appr0ve@users.noreply.github.com>
2024-10-26 09:02:04 +0300

    GNUize style

--- librdb.c
+++ librdb.c
@@ -6,15 +6,13 @@
 #include <json-glib/json-glib.h>
 
 G_DEFINE_TYPE (RdbApi, rdb_api, G_TYPE_OBJECT)
-
-static void
-rdb_api_constructed (GObject *obj)
+     static void rdb_api_constructed (GObject * obj)
 {
   G_OBJECT_CLASS (rdb_api_parent_class)->constructed (obj);
 }
 
 static void
-rdb_api_finalize (GObject *obj)
+rdb_api_finalize (GObject * obj)
 {
   RdbApi *self = RDB_API (obj);
 
@@ -34,20 +32,19 @@ typedef enum
 static GParamSpec *obj_properties[N_PROPERTIES] = { NULL, };
 
 static void
-rdb_api_set_property (GObject      *object,
-                      guint         property_id,
-                      const GValue *value,
-                      GParamSpec   *pspec)
+rdb_api_set_property (GObject * object,
+		      guint property_id,
+		      const GValue * value, GParamSpec * pspec)
 {
   RdbApi *self = RDB_API (object);
 
   switch ((RdbApiProperty) property_id)
-  {
+    {
     case RDB_URL:
       g_free (self->url);
       self->url = g_value_dup_string (value);
       break;
-     case METHOD_BINARY:
+    case METHOD_BINARY:
       g_free (self->method_binary);
       self->method_binary = g_value_dup_string (value);
       break;
@@ -55,40 +52,38 @@ rdb_api_set_property (GObject      *object,
       g_free (self->method_branch);
       self->method_branch = g_value_dup_string (value);
       break;
-  default:
+    default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
       break;
-  }
+    }
 }
 
 static void
-rdb_api_get_property (GObject      *object,
-                      guint         property_id,
-                      GValue *value,
-                      GParamSpec   *pspec)
+rdb_api_get_property (GObject * object,
+		      guint property_id, GValue * value, GParamSpec * pspec)
 {
   RdbApi *self = RDB_API (object);
 
   switch ((RdbApiProperty) property_id)
-  {
+    {
     case RDB_URL:
-      g_value_set_string ((GValue*)value, self->url);
+      g_value_set_string ((GValue *) value, self->url);
       break;
     case METHOD_BINARY:
-      g_value_set_string ((GValue*)value, self->method_binary);
+      g_value_set_string ((GValue *) value, self->method_binary);
       break;
     case METHOD_BRANCH:
-      g_value_set_string ((GValue*)value, self->method_branch);
+      g_value_set_string ((GValue *) value, self->method_branch);
       break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
       break;
-  }
+    }
 }
 
 
 static void
-rdb_api_class_init (RdbApiClass *klass)
+rdb_api_class_init (RdbApiClass * klass)
 {
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
 
@@ -96,32 +91,31 @@ rdb_api_class_init (RdbApiClass *klass)
   object_class->get_property = rdb_api_get_property;
   obj_properties[RDB_URL] =
     g_param_spec_string ("url",
-                         "URL",
-                         "RDB URL used for get requests",
-                         "https://rdb.altlinux.org/api/",
-                         G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
+			 "URL",
+			 "RDB URL used for get requests",
+			 "https://rdb.altlinux.org/api/",
+			 G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
   obj_properties[METHOD_BINARY] =
     g_param_spec_string ("method-binary",
-                         "BINARY",
-                         "Method name for get binary",
-                         "export/branch_binary_packages",
-                         G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
+			 "BINARY",
+			 "Method name for get binary",
+			 "export/branch_binary_packages",
+			 G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
   obj_properties[METHOD_BRANCH] =
     g_param_spec_string ("method-branch",
-                         "BRANCH",
-                         "Method name for get list of available branches",
-                         "errata/errata_branches",
-                         G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
+			 "BRANCH",
+			 "Method name for get list of available branches",
+			 "errata/errata_branches",
+			 G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
   g_object_class_install_properties (object_class,
-                                     N_PROPERTIES,
-                                     obj_properties);
+				     N_PROPERTIES, obj_properties);
 
   object_class->constructed = rdb_api_constructed;
   object_class->finalize = rdb_api_finalize;
 }
 
 static void
-rdb_api_init (RdbApi *self)
+rdb_api_init (RdbApi * self)
 {
   self->control_status = FALSE;
   self->target_status = FALSE;
@@ -130,23 +124,24 @@ rdb_api_init (RdbApi *self)
 }
 
 void
-rdb_api_get_branches(RdbApi *self, GError **error)
+rdb_api_get_branches (RdbApi * self, GError ** error)
 {
   g_return_if_fail (RDB_IS_API (self));
   g_return_if_fail (error == NULL || *error == NULL);
 
-  GFile * branches;
+  GFile *branches;
   g_assert (self->url != NULL);
-  gchar * errata;
-  errata = g_build_path ("/", (gchar*)self->url, "errata/errata_branches", NULL);
+  gchar *errata;
+  errata =
+    g_build_path ("/", (gchar *) self->url, "errata/errata_branches", NULL);
   g_assert (errata != NULL);
   branches = g_file_new_for_uri (errata);
-  GError * err = NULL;
+  GError *err = NULL;
   GFileInputStream *stream = g_file_read (branches, NULL, &err);
   g_assert_no_error (err);
 
-  JsonParser * parser;
-  JsonNode   * node;
+  JsonParser *parser;
+  JsonNode *node;
 
   parser = json_parser_new ();
   json_parser_load_from_stream (parser, G_INPUT_STREAM (stream), NULL, &err);
@@ -155,35 +150,37 @@ rdb_api_get_branches(RdbApi *self, GError **error)
 
   node = json_parser_get_root (parser);
   g_assert (node != NULL);
-  g_assert (JSON_NODE_HOLDS_OBJECT(node));
+  g_assert (JSON_NODE_HOLDS_OBJECT (node));
 
-  JsonObject * obj = json_node_get_object (node);
+  JsonObject *obj = json_node_get_object (node);
 
-  JsonArray * array = json_object_get_array_member (obj, "branches");
+  JsonArray *array = json_object_get_array_member (obj, "branches");
 
-  gint count = json_array_get_length(array);
+  gint count = json_array_get_length (array);
   gint i;
-  for (i = 0; i < count; i++) {
-    g_print ("%s\n", json_array_get_string_element(array, i));
-  }
+  for (i = 0; i < count; i++)
+    {
+      g_print ("%s\n", json_array_get_string_element (array, i));
+    }
 }
 
 void
-rdb_api_get_arches(RdbApi *self, GError **error)
+rdb_api_get_arches (RdbApi * self, GError ** error)
 {
   g_return_if_fail (RDB_IS_API (self));
   g_return_if_fail (error == NULL || *error == NULL);
 
-  GFile * arches;
-  const gchar * errata;
-  errata = g_build_path ("/", self->url, "site/all_pkgset_archs?branch=p10", NULL);
+  GFile *arches;
+  const gchar *errata;
+  errata =
+    g_build_path ("/", self->url, "site/all_pkgset_archs?branch=p10", NULL);
   arches = g_file_new_for_uri (errata);
-  GError * err = NULL;
+  GError *err = NULL;
   GFileInputStream *stream = g_file_read (arches, NULL, &err);
   g_assert_no_error (err);
 
-  JsonParser * parser;
-  JsonNode   * node;
+  JsonParser *parser;
+  JsonNode *node;
 
   parser = json_parser_new ();
   json_parser_load_from_stream (parser, G_INPUT_STREAM (stream), NULL, &err);
@@ -192,47 +189,48 @@ rdb_api_get_arches(RdbApi *self, GError **error)
 
   node = json_parser_get_root (parser);
   g_assert (node != NULL);
-  g_assert (JSON_NODE_HOLDS_OBJECT(node));
+  g_assert (JSON_NODE_HOLDS_OBJECT (node));
 
-  JsonObject * obj = json_node_get_object (node);
+  JsonObject *obj = json_node_get_object (node);
 
-  JsonArray * array = json_object_get_array_member (obj, "archs");
+  JsonArray *array = json_object_get_array_member (obj, "archs");
   g_assert (array != NULL);
 
-  gint count = json_array_get_length(array);
+  gint count = json_array_get_length (array);
   gint i;
-  JsonNode * nod[count];
-  JsonObject * objj[count];
-  for (i = 0; i < count; i++) {
-    nod[i] = json_array_get_element (array, i);
-    g_assert (JSON_NODE_HOLDS_OBJECT (nod[i]));
-    objj[i] = json_node_get_object (nod[i]);
-    g_print ("%s\n", json_object_get_string_member (objj[i], "arch"));
-  }
+  JsonNode *nod[count];
+  JsonObject *objj[count];
+  for (i = 0; i < count; i++)
+    {
+      nod[i] = json_array_get_element (array, i);
+      g_assert (JSON_NODE_HOLDS_OBJECT (nod[i]));
+      objj[i] = json_node_get_object (nod[i]);
+      g_print ("%s\n", json_object_get_string_member (objj[i], "arch"));
+    }
 }
 
 
 GAsyncReadyCallback
-rdb_api_binary_accepted
-(GObject *source_object, GAsyncResult *res, gpointer user_data)
+  rdb_api_binary_accepted
+  (GObject * source_object, GAsyncResult * res, gpointer user_data)
 {
   return 0;
 }
 
 
 void
-rdb_api_cache_check
-(RdbApi *self, GError **error, gchar *control, gchar *target)
+  rdb_api_cache_check
+  (RdbApi * self, GError ** error, gchar * control, gchar * target)
 {
   g_return_if_fail (RDB_IS_API (self));
   g_return_if_fail (error == NULL || *error == NULL);
 
-  GFile * ctl_file;
-  GFile * tgt_file;
+  GFile *ctl_file;
+  GFile *tgt_file;
 
-  control = g_build_path ("/", g_getenv("HOME"), ".cache", control, NULL);
+  control = g_build_path ("/", g_getenv ("HOME"), ".cache", control, NULL);
   ctl_file = g_file_new_for_path (control);
-  target = g_build_path ("/", g_getenv("HOME"), ".cache", target, NULL);
+  target = g_build_path ("/", g_getenv ("HOME"), ".cache", target, NULL);
   tgt_file = g_file_new_for_path (target);
 
   if (g_file_query_exists (ctl_file, NULL))
@@ -240,61 +238,70 @@ rdb_api_cache_check
   if (g_file_query_exists (tgt_file, NULL))
     self->target_status = TRUE;
 }
+
 void
-rdb_api_compare_binary
-(RdbApi *self, GError **error, gchar *control, gchar *target)
+  rdb_api_compare_binary
+  (RdbApi * self, GError ** error, gchar * control, gchar * target)
 {
   g_return_if_fail (RDB_IS_API (self));
   g_return_if_fail (error == NULL || *error == NULL);
   gchar *control_path, *target_path;
-  control_path = g_build_path ("/", g_getenv("HOME"), "/.cache/", control, NULL);
-  target_path = g_build_path ("/", g_getenv("HOME"), "/.cache/", target, NULL);
+  control_path =
+    g_build_path ("/", g_getenv ("HOME"), "/.cache/", control, NULL);
+  target_path =
+    g_build_path ("/", g_getenv ("HOME"), "/.cache/", target, NULL);
 }
 
 goffset progress_counter;
 
-GFileProgressCallback progress_callback (
-  goffset current_num_bytes,
-  goffset total_num_bytes,
-  gpointer user_data)
+GFileProgressCallback
+progress_callback (goffset current_num_bytes,
+		   goffset total_num_bytes, gpointer user_data)
 {
   progress_counter = current_num_bytes / 1024;
   total_num_bytes /= 1024;
-  g_print ("%s: %ld / %ld\r", (gchar*) user_data, progress_counter, total_num_bytes);
+  g_print ("%s: %ld / %ld\r", (gchar *) user_data, progress_counter,
+	   total_num_bytes);
   return 0;
 }
 
-void rdb_api_get_binary(RdbApi *self, gchar *branch)
+void
+rdb_api_get_binary (RdbApi * self, gchar * branch)
 {
-  GFile * binary_data;
-  GFile * output_data;
-  gchar * path;
-  path = g_build_path ("/", self->url, "export/branch_binary_packages", branch, NULL);
+  GFile *binary_data;
+  GFile *output_data;
+  gchar *path;
+  path =
+    g_build_path ("/", self->url, "export/branch_binary_packages", branch,
+		  NULL);
   binary_data = g_file_new_for_uri (path);
-  GError * err = NULL;
-  gchar * branch_path;
-  branch_path = g_build_path ("/", g_getenv("HOME"), "/.cache/", branch, NULL);
+  GError *err = NULL;
+  gchar *branch_path;
+  branch_path =
+    g_build_path ("/", g_getenv ("HOME"), "/.cache/", branch, NULL);
   output_data = g_file_new_for_path (branch_path);
   g_assert_no_error (err);
 
-  progress_counter = (goffset) &self->download_counter;
+  progress_counter = (goffset) & self->download_counter;
   g_file_copy (binary_data, output_data, G_FILE_COPY_OVERWRITE,
-		  NULL, (GFileProgressCallback) progress_callback, branch, &err);
+	       NULL, (GFileProgressCallback) progress_callback, branch, &err);
   g_assert_no_error (err);
-  g_object_unref(binary_data);
+  g_object_unref (binary_data);
 }
 
 void
-rdb_api_get_binaries
-(RdbApi *self, GError **error, gchar *control, gchar *target)
+  rdb_api_get_binaries
+  (RdbApi * self, GError ** error, gchar * control, gchar * target)
 {
   g_return_if_fail (RDB_IS_API (self));
   g_return_if_fail (error == NULL || *error == NULL);
 
-  if (!(self->control_status) || self->control_overwrite) {
-    rdb_api_get_binary (self, control);
-  }
-  if (!(self->target_status) || self->target_overwrite) {
-    rdb_api_get_binary (self, control);
-  }
+  if (!(self->control_status) || self->control_overwrite)
+    {
+      rdb_api_get_binary (self, control);
+    }
+  if (!(self->target_status) || self->target_overwrite)
+    {
+      rdb_api_get_binary (self, control);
+    }
 }
--- rdb.c
+++ rdb.c
@@ -30,58 +30,58 @@
  * Notice: GOptionContext generate help option itself.
  */
 gpointer print_version ();
-gpointer print_branches (RdbApi*);
-gchar * get_property (RdbApi *api, gchar *property);
+gpointer print_branches (RdbApi *);
+gchar *get_property (RdbApi * api, gchar * property);
 
 static gboolean print = FALSE;
 static gboolean list_arches, list_branches = FALSE;
 static gboolean compare_packages,
-		compare_packages_reverse,
-		compare_versions = FALSE;
-static gchar * control = NULL;
-static gchar * target = NULL;
+  compare_packages_reverse, compare_versions = FALSE;
+static gchar *control = NULL;
+static gchar *target = NULL;
 
-static const GOptionEntry entries[] =
-{
-  { "version", 'v', 0,
-    G_OPTION_ARG_NONE, &print,
-    N_("Print version"), NULL },
-  { "list-branches", 'b', 0,
-    G_OPTION_ARG_NONE, &list_branches,
-    N_("List availaible branches"), NULL },
-  { "list-arches", 'a', 0,
-    G_OPTION_ARG_NONE, &list_arches,
-    N_("List CPU arch-s available"), NULL },
-  { "compare-packages", 'p', 0,
-    G_OPTION_ARG_NONE, &compare_packages,
-    N_("Show difference existence between packages within two branches"),
-    NULL },
-  { "compare-packages-reverse", 'r', 0,
-    G_OPTION_ARG_NONE, &compare_packages_reverse,
-    N_("Reverse-order mode"), NULL },
-  { "compare-versions", 'u', 0,
-    G_OPTION_ARG_NONE, &compare_versions,
-    N_("Show difference version between packages within two branches"),
-    NULL },
-  { "control", '1', 0, 
-    G_OPTION_ARG_STRING, &control,
-    N_("Control branch"), N_("BRANCH") },
-  { "target", '2', 0,
-    G_OPTION_ARG_STRING, &target,
-    N_("Target branch"), N_("BRANCH") },
-  { NULL }
+static const GOptionEntry entries[] = {
+  {"version", 'v', 0,
+   G_OPTION_ARG_NONE, &print,
+   N_("Print version"), NULL},
+  {"list-branches", 'b', 0,
+   G_OPTION_ARG_NONE, &list_branches,
+   N_("List availaible branches"), NULL},
+  {"list-arches", 'a', 0,
+   G_OPTION_ARG_NONE, &list_arches,
+   N_("List CPU arch-s available"), NULL},
+  {"compare-packages", 'p', 0,
+   G_OPTION_ARG_NONE, &compare_packages,
+   N_("Show difference existence between packages within two branches"),
+   NULL},
+  {"compare-packages-reverse", 'r', 0,
+   G_OPTION_ARG_NONE, &compare_packages_reverse,
+   N_("Reverse-order mode"), NULL},
+  {"compare-versions", 'u', 0,
+   G_OPTION_ARG_NONE, &compare_versions,
+   N_("Show difference version between packages within two branches"),
+   NULL},
+  {"control", '1', 0,
+   G_OPTION_ARG_STRING, &control,
+   N_("Control branch"), N_("BRANCH")},
+  {"target", '2', 0,
+   G_OPTION_ARG_STRING, &target,
+   N_("Target branch"), N_("BRANCH")},
+  {NULL}
 };
 
-int main (int argc, gchar *argv[]) {
+int
+main (int argc, gchar * argv[])
+{
   GError *error = NULL;
   /* We are using that for parsing cli options */
   GOptionContext *context;
 
   RdbApi *api = g_object_new (RDB_TYPE_API, NULL);
 
-  const gchar * RDB_API_URL = g_getenv("RDB_API_URL");
-  if(RDB_API_URL)
-    api->url = (gchar*) RDB_API_URL;
+  const gchar *RDB_API_URL = g_getenv ("RDB_API_URL");
+  if (RDB_API_URL)
+    api->url = (gchar *) RDB_API_URL;
 
   /* Initialize gettext routines */
   setlocale (LC_ALL, "");
@@ -96,75 +96,83 @@ int main (int argc, gchar *argv[]) {
 
   /* Validate recieved options */
   if (!g_option_context_parse (context, &argc, &argv, &error))
-  {
-    g_print ("%s: %s\n", _("Option parsing failed"), error->message);
-    exit (1);
-  }
+    {
+      g_print ("%s: %s\n", _("Option parsing failed"), error->message);
+      exit (1);
+    }
 
   if (print)
-  {
-    print_version();
-  }
-  if
-  (control && target) {
-    if (g_strcmp0(control, target)) {
-    if (compare_packages ||
-	compare_packages_reverse ||
-	compare_versions)
     {
-      g_print("%s: %s\n", _("Using following url"), get_property(api, "url"));
-      rdb_api_cache_check (api, &error, control, target);
-      if (api->control_status) {
-	g_print ("%s %s %s",
-	       _("Cache for branch"),
-	         control,
-	       _("already exist, redownload?\n"));
-	char decision;
-        g_print ("[y/n] ");
-	while (!(decision == 'y' ||
-		 decision == 'n' ||
-		 decision == 'Y' ||
-		 decision == 'N' )) {
-	  scanf ("%c", &decision);
-          if (decision == 'y' || decision == 'Y') {
-            api->control_overwrite = TRUE;
-          }
-	}
-      }
-      if (api->target_status) {
-        g_print ("%s %s %s",
-	       _("Cache for branch"),
-	         target,
-	       _("already exist, redownload?\n"));
-        g_print ("[y/n] ");
-	char decision = '0';
-	while (!(decision == 'y' ||
-		 decision == 'n' ||
-		 decision == 'Y' ||
-		 decision == 'N' )) {
-	  scanf ("%c", &decision);
-          if (decision == 'y' || decision == 'Y') {
-            api->target_overwrite = TRUE;
-          }
-	}
-      }
-
-      g_print (_("Starting download binaries data...\n"));
-      rdb_api_get_binaries (api, &error, control, target);
-      rdb_api_compare_binary (api, &error, control, target);
-    } else
-      g_print (_("Select what to do with branches!\n"));
-    } else {
-      g_print (_("Branches must be differ!\n"));
-      return EXIT_FAILURE;
+      print_version ();
     }
-  }
-  if (list_branches) {
-    rdb_api_get_branches(api, &error);
-  }
-  if (list_arches) {
-    rdb_api_get_arches(api, &error);
-  }
+  if (control && target)
+    {
+      if (g_strcmp0 (control, target))
+	{
+	  if (compare_packages ||
+	      compare_packages_reverse || compare_versions)
+	    {
+	      g_print ("%s: %s\n", _("Using following url"),
+		       get_property (api, "url"));
+	      rdb_api_cache_check (api, &error, control, target);
+	      if (api->control_status)
+		{
+		  g_print ("%s %s %s",
+			   _("Cache for branch"),
+			   control, _("already exist, redownload?\n"));
+		  char decision;
+		  g_print ("[y/n] ");
+		  while (!(decision == 'y' ||
+			   decision == 'n' ||
+			   decision == 'Y' || decision == 'N'))
+		    {
+		      scanf ("%c", &decision);
+		      if (decision == 'y' || decision == 'Y')
+			{
+			  api->control_overwrite = TRUE;
+			}
+		    }
+		}
+	      if (api->target_status)
+		{
+		  g_print ("%s %s %s",
+			   _("Cache for branch"),
+			   target, _("already exist, redownload?\n"));
+		  g_print ("[y/n] ");
+		  char decision = '0';
+		  while (!(decision == 'y' ||
+			   decision == 'n' ||
+			   decision == 'Y' || decision == 'N'))
+		    {
+		      scanf ("%c", &decision);
+		      if (decision == 'y' || decision == 'Y')
+			{
+			  api->target_overwrite = TRUE;
+			}
+		    }
+		}
+
+	      g_print (_("Starting download binaries data...\n"));
+	      rdb_api_get_binaries (api, &error, control, target);
+	      rdb_api_compare_binary (api, &error, control, target);
+	    }
+	  else
+	    g_print (_("Select what to do with branches!\n"));
+	}
+      else
+	{
+	  g_print (_("Branches must be differ!\n"));
+	  return EXIT_FAILURE;
+	}
+    }
+  if (list_branches)
+    {
+      rdb_api_get_branches (api, &error);
+    }
+  if (list_arches)
+    {
+      rdb_api_get_arches (api, &error);
+    }
 
   g_option_context_free (context);
 
@@ -174,18 +182,22 @@ int main (int argc, gchar *argv[]) {
 /* Print version number
  * options: -v --version
  */
-gpointer print_version () {
+gpointer
+print_version ()
+{
   g_print ("%s %s\n", GETTEXT_PACKAGE, VERSION);
   return NULL;
 }
 
-gchar * get_property (RdbApi *api, gchar *property) {
+gchar *
+get_property (RdbApi * api, gchar * property)
+{
   gchar *value;
   GValue val = G_VALUE_INIT;
   g_value_init (&val, G_TYPE_STRING);
-  g_object_get_property(G_OBJECT (api), property, &val);
+  g_object_get_property (G_OBJECT (api), property, &val);
   g_assert (G_VALUE_HOLDS_STRING (&val));
-  value = g_strdup (g_value_get_string(&val));
+  value = g_strdup (g_value_get_string (&val));
   g_value_unset (&val);
   return value;
 }
