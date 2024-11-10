a2fc567ae77c7a3f3c6eeb51312fa7b4e2c2b808
appr0ve <185701845+appr0ve@users.noreply.github.com>
2024-11-10 19:28:23 +0300

    autogen.sh and gear rules update

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-function prepare () {
-  su --login \
-    apt-get install -y \
-    gcc meson glib2-devel libjson-glib-devel libgio-devel
-}
-function get () {
-  wget 'https://savannah.gnu.org/cgi-bin/viewcvs/*checkout*/config/config/config.{guess,sub}'
-}
+
+if !(rpm -qa | grep automake); then
+ su --login \
+   apt-get install -y \
+   gcc automake autoconf gettext \
+   glib2-devel libjson-glib-devel libgio-devel
+fi
+
 autoreconf -i
